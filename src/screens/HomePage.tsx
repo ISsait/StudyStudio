2f7091e2859980c660410d4a491a757497d8e21f
ISsait <152314143+ISsait@users.noreply.github.com>
2024-11-22 08:30:48 -0700

    calendar working

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -4,11 +4,13 @@ import {
     Text,
 } from 'react-native';
 import { commonStyles } from '../commonStyles';
+import CalendarComponent from '../components/homeComponents/Calendar';
 
 export default function HomePage () : React.JSX.Element {
     return (
-        <View>
-            <Text style={commonStyles.body}>Home Page Body</Text>
+        <View style={commonStyles.body}>
+            <Text>Home Page Body</Text>
+            <CalendarComponent />
         </View>
     );
 }
---
+++ src/utility.tsx
@@ -0,0 +1,13 @@
+// List of colors for courses
+export const courseColors =
+    {
+        pink:'#FFD1DC',
+        blue: '#AEC6CF',
+        green: '#D5F5E3',
+        yellow: '#FFFACD',
+        purple: '#D6BDFD',
+        peach: '#FFDAB9',
+        mint: '#BDFCC9',
+        lavender: '#E6E6FA',
+    };
+
